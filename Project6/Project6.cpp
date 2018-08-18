6dde8bd57dcf63f4c39afd250057e445cd22dccc
Jordan Graves <jordangravesba@icloud.com>
2018-08-18 12:48:10 -0500

    Add files via upload

---
+++ Project6/Project6.cpp
@@ -0,0 +1,46 @@
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include "lua.h"
+
+
+
+int main() { return 0; }
+
+
+using namespace std;
+
+
+namespace PrintExploit {
+	namespace Integers {
+		int printtype = 1; // 0 = Print, 1 = Info, 2 = Warn, 3 = Error
+	}
+}
+
+
+
+void printy() {
+
+	console("Printy"); // Title
+	cout << "Printy Loaded! \n"; // Start Up Messages
+	cout << "Welcome to Printy, Created by Klepto, with the help of Karky \n"; // Start Up Messages
+	cout << "\n";
+	cout << "Printy Ready ~ You May Now Print \n";
+	cout << "\n";
+	do {
+		
+		string outstr = "";
+		getline(cin, outstr);
+		SingletonPrint(PrintExploit::Integers::printtype, outstr.c_str());
+		cout << "Printed Successfully! \n"; // Success Print Message
+	} while (true);
+}
+
+
+int __stdcall DllMain(HMODULE DLL, DWORD Reason, void* Useless) {
+	if (Reason == 1) {
+		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)printy, 0, 0, 0);
+	}
+	return TRUE;
+}
+
---
+++ Project6/lua.h
@@ -0,0 +1,31 @@
+#include <Windows.h>
+#include <iostream>
+#include <string>
+
+
+int Address(int Offset) {
+	return (Offset - 0x00400000 + (DWORD)GetModuleHandle("RobloxPlayerBeta.exe"));
+}
+
+
+typedef int(__cdecl *SINGLETON)(int a1, const char *a2);
+SINGLETON SingletonPrint = (SINGLETON)Address(0x51EB70);
+
+
+void cnsb() {
+	DWORD CheckerValue;
+	VirtualProtect((PVOID)&FreeConsole, 1, PAGE_EXECUTE_READWRITE, &CheckerValue);
+	*(BYTE*)(&FreeConsole) = (0xC3);
+}
+
+
+void console(const char* csttl) {
+	cnsb();
+	AllocConsole();
+	SetConsoleTitleA(csttl);
+	freopen("CONOUT$", "w", stdout);
+	freopen("CONIN$", "r", stdin);
+	HWND ConsoleHandle = GetConsoleWindow();
+	::SetWindowPos(ConsoleHandle, HWND_TOPMOST, 0, 0, 0, 0, SWP_DRAWFRAME | SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+	::ShowWindow(ConsoleHandle, SW_NORMAL);
+}
